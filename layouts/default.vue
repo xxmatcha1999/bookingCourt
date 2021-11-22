129caee4c53c1efe89c338f460c87256f474379d
folkspch <folk_spch@hotmail.com>
2021-11-23 04:30:03 +0700

    timetable

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,12 +1,22 @@
 <template>
-  <v-app dark>
+  <v-app light style="background-color: white">
     <v-navigation-drawer
       v-model="drawer"
       :mini-variant="miniVariant"
       :clipped="clipped"
       fixed
       app
+      color="#92b9e7"
+      
     >
+      <v-row
+        justify="space-around"
+        style="margin-top: 30px; margin-bottom: 30px"
+      >
+        <v-avatar color="white" size="120">
+          <img src="https://www.kindpng.com/picc/m/105-1055656_account-user-profile-avatar-avatar-user-profile-icon.png" alt="profilePic" />
+        </v-avatar>
+      </v-row>
       <v-list>
         <v-list-item
           v-for="(item, i) in items"
@@ -24,94 +34,77 @@
         </v-list-item>
       </v-list>
     </v-navigation-drawer>
-    <v-app-bar
-      :clipped-left="clipped"
-      fixed
-      app
-    >
+    <v-app-bar :clipped-left="clipped" fixed app color="#92b9e7">
       <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-      <v-btn
-        icon
-        @click.stop="miniVariant = !miniVariant"
-      >
-        <v-icon>mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="clipped = !clipped"
-      >
-        <v-icon>mdi-application</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="fixed = !fixed"
-      >
-        <v-icon>mdi-minus</v-icon>
-      </v-btn>
-      <v-toolbar-title v-text="title" />
-      <v-spacer />
-      <v-btn
-        icon
-        @click.stop="rightDrawer = !rightDrawer"
-      >
-        <v-icon>mdi-menu</v-icon>
-      </v-btn>
+      <v-spacer/>
+      <a style="color:white;margin-right:0;">ออกจากระบบ</a>
     </v-app-bar>
     <v-main>
       <v-container>
         <Nuxt />
       </v-container>
     </v-main>
-    <v-navigation-drawer
-      v-model="rightDrawer"
-      :right="right"
-      temporary
-      fixed
-    >
+    <v-navigation-drawer v-model="rightDrawer" :right="right" temporary fixed>
       <v-list>
         <v-list-item @click.native="right = !right">
           <v-list-item-action>
-            <v-icon light>
-              mdi-repeat
-            </v-icon>
+            <v-icon light> mdi-repeat </v-icon>
           </v-list-item-action>
           <v-list-item-title>Switch drawer (click me)</v-list-item-title>
         </v-list-item>
       </v-list>
     </v-navigation-drawer>
-    <v-footer
-      :absolute="!fixed"
-      app
-    >
-      <span>&copy; {{ new Date().getFullYear() }}</span>
+    <v-footer :absolute="!fixed" app>
+      <span>KMUTNB &copy; {{ new Date().getFullYear() }}</span>
     </v-footer>
   </v-app>
 </template>
 
 <script>
 export default {
-  data () {
+  data() {
     return {
       clipped: false,
       drawer: false,
       fixed: false,
       items: [
         {
-          icon: 'mdi-apps',
-          title: 'Welcome',
-          to: '/'
+          icon: "mdi-apps",
+          title: "หน้าแรก",
+          to: "/",
         },
         {
-          icon: 'mdi-chart-bubble',
-          title: 'Inspire',
-          to: '/inspire'
-        }
+          icon: "mdi-timetable",
+          title: "ตารางเวลา",
+          to: "/timetable",
+        },
+        {
+          icon: "mdi-order-bool-descending",
+          title: "กฏระเบียบ",
+          to: "/rules",
+        },
+        {
+          icon: "mdi-clipboard-text-clock-outline",
+          title: "จองสนาม",
+          to: "/booking",
+        },
+        {
+          icon: "mdi-contacts",
+          title: "ติดต่อ",
+          to: "/contact",
+        },
       ],
       miniVariant: false,
       right: true,
       rightDrawer: false,
-      title: 'Vuetify.js'
-    }
-  }
-}
+      title: "Vuetify.js",
+    };
+  },
+};
 </script>
+<style scoped>
+@import url("https://fonts.googleapis.com/css2?family=Prompt:wght@100;300;500&display=swap");
+* {
+  font-family: "Prompt", sans-serif;
+}
+</style>
---
+++ pages/timetable.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <v-card
+      style="padding: 40px"
+      max-width="50%"
+      class="d-flex mx-auto my-6"
+      light
+    >
+      <v-col cols="3">
+        <img
+          width="150"
+          src="https://img.lovepik.com/photo/50154/0220.jpg_wh860.jpg"
+        />
+      </v-col>
+      <v-col cols="5">
+        <v-card-title>สนามบาสเกตบอล</v-card-title>
+        <v-btn>เลือก</v-btn>
+      </v-col>
+    </v-card>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+</style>
